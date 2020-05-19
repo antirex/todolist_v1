c3b691e8e3ec8b67ccf6113e512b7c3564e1bb34
antirex <anshulbamb22@gmail.com>
2020-05-19 17:32:12 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const bodyParser =require("body-parser");
+const port = 3000;
+const app = express();
+var items =["Stay Positive!"];
+app.use(bodyParser.urlencoded({extended:true}));
+app.set("view engine","ejs"); //tells our app to use ejs as our view-engine ...necessary to be under const app;
+app.use(express.static("css"));
+app.get("/",function(req,res){
+    var day = new Date();
+   
+
+    var options ={
+        weekday : "long",
+        day: "numeric",
+        month: "long"
+    }
+    var today =day.toLocaleDateString("en-US",options);
+
+    res.render("list",{weekDay: today, newItem: items});
+    res.send();
+})
+
+app.post("/",function(req,res){
+    var item = req.body.addItem;
+    items.push(item);
+    res.redirect("/");
+    
+})
+
+app.listen(process.env.PORT || 3000,function(){
+    console.log("your server is live at https://localhost:"+port);
+    
+})
